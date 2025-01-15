5b7c23750e7a8adbc954a8d8bacd46fd18f9ba52
samuel-melo1 <samuelprogramacao3@gmail.com>
2025-01-14 22:34:50 -0300

    implementation schedule method

--- src/main/java/com/samuel/notificationscheduler/NotificationSchedulerApplication.java
+++ src/main/java/com/samuel/notificationscheduler/NotificationSchedulerApplication.java
@@ -2,8 +2,10 @@ package com.samuel.notificationscheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NotificationSchedulerApplication {
 
     public static void main(String[] args) {
