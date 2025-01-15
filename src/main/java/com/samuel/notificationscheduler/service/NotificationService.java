6b4588678cda7121896b8ae0db58c620e329fb44
samuel-melo1 <samuelprogramacao3@gmail.com>
2025-01-14 22:35:58 -0300

    implementated query jpa, cancel notification and send notification

--- src/main/java/com/samuel/notificationscheduler/service/NotificationService.java
+++ src/main/java/com/samuel/notificationscheduler/service/NotificationService.java
@@ -1,8 +1,15 @@
 package com.samuel.notificationscheduler.service;
 
 import com.samuel.notificationscheduler.controller.dto.ScheduleNotificationDto;
+import com.samuel.notificationscheduler.entity.Notification;
+import com.samuel.notificationscheduler.enums.StatusEnum;
 import com.samuel.notificationscheduler.repository.NotificationRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
 
 @Service
 public class NotificationService {
@@ -15,5 +22,33 @@ public class NotificationService {
 
     public void scheduleNotification(ScheduleNotificationDto dto){
         repository.save(dto.toNotification());
+    }
+    public Optional<Notification> findById(Long notificationId){
+        return repository.findById(notificationId);
+    }
+
+    public void cancelNotification(Long notificationId){
+       var notification = findById(notificationId);
+
+       if(notification.isPresent()){
+           notification.get().setStatus(StatusEnum.CANCELED.toStatus());
+           repository.save(notification.get());
+       }
+    }
+
+    public void checkAndSend(LocalDateTime dateTime){
+       var notifications = repository.findByStatusInAndDateTimeBefore(List.of(
+               StatusEnum.PENDENTE.toStatus(), StatusEnum.ERROR.toStatus()), dateTime);
+
+       notifications.forEach(sendNotification());
+    }
+
+    private  Consumer<Notification> sendNotification() {
+        return n -> {
+            // Realizar o envio da notificação
+
+            n.setStatus(StatusEnum.SUCCESS.toStatus());
+            repository.save(n);
+        };
     }
 }
