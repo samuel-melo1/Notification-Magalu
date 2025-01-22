c89e617e6e8691660436eda95a04ee2137021a79
samuel-melo1 <samuelprogramacao3@gmail.com>
2025-01-21 23:37:52 -0300

    initializate monorepo

--- src/main/java/com/samuel/notificationscheduler/repository/StatusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.samuel.notificationscheduler.repository;
-
-import com.samuel.notificationscheduler.entity.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusRepository extends JpaRepository<Status, Long> {
-}
